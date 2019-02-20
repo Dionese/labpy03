ea031e6b91d27d6c833788df5dd3e70cebf22761
Dion baharudin <45952796+Dionese@users.noreply.github.com>
2019-02-20 23:35:04 +0700

    Add files via upload

---
+++ latihan2.py
@@ -0,0 +1,7 @@
+print("menampilkan bilangan berhenti ketika bilangan 0,dan menampilkan bilangan terbesar")
+b = 0
+while True:
+    angka = int(input("Masukan bilangan : "))
+    b = angka
+    if b == 0:
+        break
